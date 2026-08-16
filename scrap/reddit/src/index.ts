8ca845616f29011bfb7c70a9465c49ea41ba02d0
anasdev1204 <anasdev1204@gmail.com>
2026-08-16 12:39:01 +0100

    feat: add basic data extraction from reddit search in r/pokemontcg

--- scrap/reddit/src/index.ts
+++ scrap/reddit/src/index.ts
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
-import type { Browser, Puppeteer } from 'puppeteer';
+import type { Browser } from 'puppeteer';
 
 
 // @ts-expect-error
@@ -11,15 +11,45 @@ await puppeteer.use(StealthPlugin())
         executablePath: "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
     })
     .then(async (browser: Browser) => {
-        const page = await browser.newPage()
-        await page.goto('https://www.reddit.com/r/PokemonTCG/')
+        const page = await browser.newPage();
+        await page.goto('https://www.reddit.com/r/PokemonTCG/');
 
-        let searchBarSelector = '[name="q"]'
-        console.log("looking for ", searchBarSelector)
-        let searchBar = await page.waitForSelector(searchBarSelector)
-        console.log("found ", searchBar)
-        await page.click(searchBarSelector)
-        await page.keyboard.type('destined rivals')
+        let searchBarSelector = '[name="q"]';
+        
+        await page.waitForSelector(searchBarSelector);
+        await page.click(searchBarSelector);
+        await page.keyboard.type('destined rivals');
+        await page.keyboard.press('Enter');
 
-        await page.keyboard.press('Enter')
-    })
+        await page.waitForNavigation();
+
+        for (let i = 0; i < 3; i++) {
+            await page.evaluate(() => {
+                window.scrollBy(0, window.innerHeight);
+            });
+
+            await new Promise(resolve => setTimeout(resolve, 1500));
+        };
+
+        const posts = await page.$$eval(
+            'div[data-testid="sdui-post-unit"]',
+            elements => elements.map((element) => {
+                const postDate = element.querySelector('time')?.getAttribute('datetime');
+            
+                let postStats = element.querySelector('div[data-testid="search-counter-row"]');
+                let postUpvotesSpan = postStats?.querySelectorAll('span')[0];
+                let postCommentsSpan = postStats?.querySelectorAll('span')[2];
+
+                const postUpvotes = postUpvotesSpan?.querySelector('faceplate-number')?.getAttribute("number");
+                const postComments = postCommentsSpan?.querySelector('faceplate-number')?.getAttribute("number");
+
+                return {
+                    postDate,
+                    postUpvotes,
+                    postComments
+                }
+            })
+        );
+
+        console.log(posts);
+    });
